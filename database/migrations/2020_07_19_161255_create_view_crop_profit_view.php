44208cab0150dcf81a365c057e3360b5f562140f
xeaneax <jakodapogi1@gmail.com>
2020-07-30 02:48:10 +0800

    after defense 3

--- database/migrations/2020_07_19_161255_create_view_crop_profit_view.php
+++ database/migrations/2020_07_19_161255_create_view_crop_profit_view.php
@@ -16,16 +16,7 @@ class CreateViewCropProfitView extends Migration
         \DB::statement("
         CREATE VIEW view_crop_profit AS
         SELECT
-            posts.id AS id,
             posts.crop_name AS crop_name,
-            posts.crop_price AS crop_price,
-            posts.crop_desc AS crop_desc,
-            posts.crop_quantity AS crop_quantity,
-            posts.crop_status AS crop_status,
-            posts.created_at AS created_at,
-            posts.updated_at AS updated_at,
-            posts.user_id AS user_id,
-            posts.crop_image AS crop_image,
             SUM(posts.kilogram_sold::decimal) AS kilogram_sold,
             SUM(posts.fixed_quantity::decimal) AS fixed_quantity,
             SUM(posts.earnings::decimal) AS earnings,
@@ -34,7 +25,7 @@ class CreateViewCropProfitView extends Migration
             SUM(posts.crop_profitability::decimal) AS crop_profitability
         FROM
             posts
-
+        GROUP BY posts.crop_name
         ORDER BY posts.crop_profitability DESC
         ");
     }
